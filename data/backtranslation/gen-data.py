dad974201d3eaad79080c0d93beec507955d1175
HitkoDev <development@hitko.si>
2023-04-24 22:47:27 +0200

    feat: use better translation

--- data/backtranslation/gen-data.py
+++ data/backtranslation/gen-data.py
@@ -1,13 +1,13 @@
 import argparse
 import csv
 import os
-import uuid
 
 import requests
 import spacy
 from nltk.tokenize import sent_tokenize
+import re
 
-endpoint = 'https://api.cognitive.microsofttranslator.com'
+endpoint = 'https://api.deepl.com/v2/translate'
 
 
 def translate(
@@ -15,7 +15,6 @@ def translate(
     to,
         language='sl',
         backtransleate=False,
-        region=os.environ.get('TRANSLATOR_TEXT_REGION', None),
         key=os.environ.get('TRANSLATOR_TEXT_SUBSCRIPTION_KEY', None)
 ):
     if not isinstance(to, list):
@@ -33,21 +32,14 @@ def translate(
     else:
         chains.append(list(zip(chain[0:-1], chain[1:])))
 
-    path = '/translate?api-version=3.0'
-
     headers = {
-        'Ocp-Apim-Subscription-Key': key,
-        'Ocp-Apim-Subscription-Region': region,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
+        'Authorization': 'DeepL-Auth-Key {}'.format(key)
     }
 
     if not isinstance(text, list):
         text = [text]
 
-    input = [{
-        'text': t
-    } for t in text]
+    input = text
     out = []
 
     mp = {}
@@ -55,22 +47,20 @@ def translate(
     for c in chains:
         for j, p in enumerate(c):
             f, t = p
-            params = '&from={}&to={}'.format(f, t)
-            k = '{}_{}'.format(params, j)
+            k = '{}_{}_{}'.format(f, t, j)
             if k in mp:
                 input = mp[k]
             else:
-                constructed_url = endpoint + path + params
-
-                request = requests.post(
-                    constructed_url, headers=headers, json=input)
+                request = requests.post(endpoint, headers=headers, data={
+                    'source_lang': f,
+                    'target_lang': t,
+                    'text': input
+                })
                 response = request.json()
 
-                input = mp[k] = [{
-                    'text': r["translations"][0]['text']
-                } for r in response]
+                input = mp[k] = [r['text'] for r in response["translations"]]
 
-        out.append([t['text'] for t in input])
+        out.append(input)
 
     return out
 
@@ -80,8 +70,6 @@ if __name__ == '__main__':
     parser.add_argument('inputfile')
     parser.add_argument('-i', '--input-language', default='sl')
     parser.add_argument(
-        '-r', '--region', default=os.environ.get('TRANSLATOR_TEXT_REGION', None))
-    parser.add_argument(
         '-k', '--key', default=os.environ.get('TRANSLATOR_TEXT_SUBSCRIPTION_KEY', None))
     parser.add_argument('-o', '--out', required=True)
     parser.add_argument('-l', '--languages', nargs='+', default=['en', 'de'])
@@ -90,13 +78,13 @@ if __name__ == '__main__':
     with open(args.inputfile, 'rb') as f:
         contents = f.read().decode('utf8')
 
-    contents = contents.replace('\n', ' ')
+    contents = re.sub(r'[\t\n\r]+', ' ', contents)
 
     nlp = spacy.load('en_core_web_trf')
 
     # sentences = [str(f).strip() for f in nlp(contents).sents if str(f).strip()]
     sentences = [str(f).strip() for f in sent_tokenize(
-        contents, language='slovene') if str(f).strip()]
+        contents, language='slovene') if str(f).strip()][0:100]
     res = [sentences]
     if len(sentences):
         res = translate(
@@ -104,8 +92,7 @@ if __name__ == '__main__':
             language=args.input_language,
             to=args.languages,
             backtransleate=True,
-            key=args.key,
-            region=args.region
+            key=args.key
         )
 
     with open(args.out, 'w', newline='', encoding='utf-8') as file:
