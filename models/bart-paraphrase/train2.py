2f687a0aab5beaf2900f9644a640b59be9d1d643
HitkoDev <development@hitko.si>
2023-05-25 23:58:54 +0200

    feat: train on common data

---
+++ models/bart-paraphrase/train2.py
@@ -0,0 +1,109 @@
+import logging
+import os
+from datetime import datetime
+
+import pandas as pd
+from simpletransformers.seq2seq import Seq2SeqModel
+from sklearn.model_selection import train_test_split
+from utils import (CustomSimpleDataset, Seq2SeqArgsFix,
+                   clean_unnecessary_spaces, load_data)
+
+# torch.multiprocessing.set_sharing_strategy('file_system')
+
+
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.ERROR)
+
+train_df = pd.concat(
+    [
+        load_data("data/pairs-train-t5-aug.csv", "sentence1",
+                  "sentence2", src_lang="sl_SI"),
+        load_data("data/pairs-train-t5-base.csv", "sentence1",
+                  "sentence2", src_lang="sl_SI"),
+    ]
+)
+eval_df = load_data("data/pairs-dev-t5.csv", "sentence1",
+                    "sentence2", src_lang="sl_SI"),
+
+train_df = train_df[["prefix", "input_text",
+                     "target_text", "src_lang", "tgt_lang"]]
+eval_df = eval_df[["prefix", "input_text",
+                   "target_text", "src_lang", "tgt_lang"]]
+
+train_df = train_df.dropna()
+eval_df = eval_df.dropna()
+
+train_df["input_text"] = train_df["input_text"].apply(clean_unnecessary_spaces)
+train_df["target_text"] = train_df["target_text"].apply(
+    clean_unnecessary_spaces)
+
+eval_df["input_text"] = eval_df["input_text"].apply(clean_unnecessary_spaces)
+eval_df["target_text"] = eval_df["target_text"].apply(clean_unnecessary_spaces)
+
+print(train_df)
+
+model_args = Seq2SeqArgsFix()
+model_args.eval_batch_size = 64
+model_args.evaluate_during_training = False
+model_args.evaluate_during_training_steps = 2500
+model_args.evaluate_during_training_verbose = True
+model_args.fp16 = False
+model_args.learning_rate = 1e-4
+model_args.max_seq_length = 128
+model_args.num_train_epochs = 5
+model_args.overwrite_output_dir = True
+model_args.reprocess_input_data = False
+model_args.use_cached_eval_features = True
+model_args.save_eval_checkpoints = False
+model_args.save_steps = -1
+model_args.train_batch_size = 8
+model_args.use_multiprocessing = False
+model_args.use_multiprocessing_for_evaluation = False
+
+model_args.do_sample = True
+model_args.num_beams = 1
+model_args.num_return_sequences = 3
+model_args.max_length = 128
+model_args.top_k = 50
+model_args.top_p = 0.95
+model_args.dataset_class = CustomSimpleDataset
+model_args.tgt_lang = 'sl_SI'
+model_args.src_lang = 'sl_SI'
+
+# model_args.wandb_project = "Paraphrasing with BART"
+
+
+model = Seq2SeqModel(
+    encoder_decoder_type="mbart50",
+    encoder_decoder_name="facebook/mbart-large-50",
+    # encoder_decoder_name="outputs/checkpoint-138068-epoch-2",
+    args=model_args,
+    # use_cuda=False,
+)
+
+model.train_model(train_df, eval_data=eval_df)
+
+to_predict = eval_df["input_text"].tolist()
+truth = eval_df["target_text"].tolist()
+
+preds = model.predict(to_predict)
+
+# Saving the predictions if needed
+os.makedirs("predictions", exist_ok=True)
+eval_df = load_data("data/pairs-test-t5.csv", "sentence1",
+                    "sentence2", src_lang="sl_SI"),
+
+with open(f"predictions/predictions_{datetime.now()}.txt", "w", encoding='utf-8') as f:
+    for i, text in enumerate(eval_df["input_text"].tolist()):
+        f.write(str(text) + "\n\n")
+
+        f.write("Truth:\n")
+        f.write(truth[i] + "\n\n")
+
+        f.write("Prediction:\n")
+        for pred in preds[i]:
+            f.write(str(pred) + "\n")
+        f.write(
+            "________________________________________________________________________________\n"
+        )
